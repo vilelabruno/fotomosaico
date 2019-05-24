2509f402f44bf6881aeac4ea3237c9229271a999
Bruno Vilela <bvilela@192.168.1.3>
2019-05-24 15:09:44 -0300

    created the image resized

--- core.py
+++ core.py
@@ -1,12 +1,13 @@
-from netpbmfile import imread
-
+from netpbmfile import imread, imsave
+import numpy
 nt = 0
 nl = 0
 nc = 0
 
 imgQtd = 0
 
-imgNames = []
+imgArr = []
+fotoMosaico = []
 
 with open("config.txt") as file:
 	i = 0
@@ -19,10 +20,12 @@ with open("config.txt") as file:
 		elif i == 1:
 			imgQtd = int(line)
 		elif i > 1:
-			imgNames.append(line.split("\n")[0])
+			imgArr.append(imread(line.split("\n")[0]))
 		i += 1
 
-baseImg = imread(imgNames[0])
+nl = nl *10
+nc = nc *10
+baseImg = imgArr[0]
 
 #print (baseImg)
 #print ((baseImg[1][2]))
@@ -30,6 +33,13 @@ baseImg = imread(imgNames[0])
 #print ((baseImg[1][0]))
 #print ((baseImg[0][0]))
 
-for x in range(0,nl-1):
-	for y in range(0,nc-1):
-		print (baseImg[x][y])
+#im1 = numpy.array([[0, 1], [65534, 65535]], dtype='uint16')
+#imsave('_tmp.pgm', im1)
+for x in range(0,nl*nl):
+	line = []
+	for y in range(0,nc*nc):
+		line.append(baseImg[int(x/nl)][int(y/nc)])
+	fotoMosaico.append(line)
+#print (len(fotoMosaico))
+fotoMosaicoPbm = numpy.array(fotoMosaico, dtype='uint16')
+imsave('fotoMosaico.pgm', fotoMosaicoPbm)
